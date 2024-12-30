888a1484b570535db1097ef9b1f94689363d798f
nandini <110680375+nandinikashyap1902@users.noreply.github.com>
2024-12-31 00:55:45 +0530

    some changes

--- company-showcase/prisma/prisma.js
+++ company-showcase/prisma/prisma.js
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
-
+import dotenv from "dotenv"
+dotenv.config();
 const prisma = global.prisma || new PrismaClient();
-
 if (process.env.NODE_ENV !== 'production') {
     global.prisma = prisma;
 }
