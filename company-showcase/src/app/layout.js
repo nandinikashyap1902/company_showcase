f229c88b58ceeff18e2dd16a51b74549ed9abfb5
nandini <110680375+nandinikashyap1902@users.noreply.github.com>
2025-01-02 22:14:01 +0530

    env.local updated

--- company-showcase/src/app/layout.js
+++ company-showcase/src/app/layout.js
@@ -3,7 +3,6 @@ export const metadata = {
     title: "Company Showcase",
     description: "A Next.js app showcasing companies and their directors",
 };
-
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
